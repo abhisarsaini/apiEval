258c266d7dde19a5428e9be34edf6289d20e70ad
Abhisar Saini <abhisar.saini@snapdeal.com>
2015-02-05 17:31:17 +0530

    basic app

--- src/main/java/evaluation/api/ApiRestService.java
+++ src/main/java/evaluation/api/ApiRestService.java
@@ -1,5 +1,16 @@
 package evaluation.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/rest")
 public class ApiRestService {
+
+	@GET
+	@Path("/testFunc")
+	public Response apiTestFunc() {
+		return Response.status(Response.Status.OK).entity("Reached").build();
+	}
 
 }
