9ed6a4740e62bbd8e974db8ed7d617c6486118b4
Daivik Gangwar <daivik.gangwar@snapdeal.com>
2015-02-12 15:38:47 +0530

    Dummy data creation

--- src/main/java/evaluation/api/ApiRestService.java
+++ src/main/java/evaluation/api/ApiRestService.java
@@ -1,7 +1,10 @@
 package evaluation.api;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/rest")
@@ -10,7 +13,21 @@ public class ApiRestService {
 	@GET
 	@Path("/testFunc")
 	public Response apiTestFunc() {
+
 		return Response.status(Response.Status.OK).entity("Reached").build();
 	}
 
+	@GET
+	@Path("/review/{sid}")
+	public Response getReviewBySellerId(@PathParam("sid") int sid) {
+		return Response.status(Response.Status.OK)
+				.entity(SimpleClient.getReviewBySellerId(sid)).build();
+	}
+
+	@PUT
+	@Path("/load")
+	public Response loadData() {
+		SimpleClient.loadData();
+		return Response.status(Response.Status.OK).entity("Loaded").build();
+	}
 }
---
+++ src/main/java/evaluation/api/SimpleClient.java
@@ -0,0 +1,92 @@
+package evaluation.api;
+
+import java.util.Random;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class SimpleClient {
+	private static Cluster cluster;
+	static Random ran = new Random();
+	static Session session;
+
+	public static void connect(String node) {
+		cluster = Cluster.builder().addContactPoint(node).build();
+		Metadata metadata = cluster.getMetadata();
+		System.out.printf("Connected to cluster: %s\n",
+				metadata.getClusterName());
+		for (Host host : metadata.getAllHosts()) {
+			System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
+					host.getDatacenter(), host.getAddress(), host.getRack());
+		}
+	}
+
+	public static void close() {
+		cluster.close();
+	}
+
+	public static String getTitle() {
+		connect("127.0.0.1");
+		session = cluster.connect();
+		String title = "";
+
+		ResultSet results = session.execute("SELECT * FROM simplex.songs");
+		for (Row row : results) {
+
+			title = row.getString("review");
+		}
+		close();
+		return title;
+	}
+
+	public static void loadData() {
+		connect("127.0.0.1");
+		session = cluster.connect();
+
+		session.execute("CREATE KEYSPACE revrat WITH replication "
+				+ "= {'class':'SimpleStrategy', 'replication_factor':3};");
+
+		session.execute("CREATE TABLE revrat.reviews (" + "id uuid,"
+				+ "sellerid int," + "productid int," + "review text,"
+				+ "rating int," + "PRIMARY KEY(sellerid, productid)," + ");");
+
+		for (int i = 1; i < 10000; i++) {
+			session.execute("INSERT INTO revrat.reviews (sellerid, productid, review, rating) "
+					+ "VALUES ("
+
+					+ ran.nextInt(100)
+					+ ","
+					+ ran.nextInt(1000)
+					+ ","
+					+ "'This ia very good product"
+					+ i
+					+ "'"
+					+ ","
+					+ ran.nextInt(5) + ");");
+		}
+
+	}
+
+	public static String getReviewBySellerId(int sid) {
+		connect("127.0.0.1");
+		session = cluster.connect();
+		String title = "";
+
+		ResultSet results = session.execute("SELECT * FROM revrat.reviews "
+				+ "WHERE sellerid = " + sid + ";");
+		for (Row row : results) {
+
+			title = row.getString("review");
+		}
+		close();
+		return title;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
